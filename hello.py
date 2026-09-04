2ae8db27856bf1cbe7c4da0535e89148738a87c3
edenl <edenlitx@gmail.com>
2026-09-04 16:58:00 +0800

    Fifth Post

--- hello.py
+++ hello.py
@@ -27,7 +27,7 @@ print("Day 3:", followers)
 
 #Activity 4 ===============
 username = input("Enter Username: ")
-age = input("Enter Age: ")
+age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
 
 print("\nInstagram Profile")
@@ -36,3 +36,6 @@ print("Username:", username)
 print("Age:", age)
 print("Category:", category)
 
+#Activity 5 ===============
+if age>40 and category == "fun":
+    print("You are old what is fun for you ?? ")
