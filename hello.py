46891cead05872b80c2d43cd798623382159471b
edenl <edenlitx@gmail.com>
2026-09-04 16:53:35 +0800

    Fourth Post

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ print("Welcome here")
 print("My first post!")
 print("=================")
 
+#Activity 2 ===============
 username = "cool_creator"
 bio = "Fun Blogger"
 followers = 100
@@ -12,6 +13,7 @@ print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
 
+#Activity 3 ===============
 followers = 100
 
 followers += 50
@@ -22,3 +24,15 @@ print("Day 2:", followers)
 
 followers -= 10
 print("Day 3:", followers)
+
+#Activity 4 ===============
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("=================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
+
