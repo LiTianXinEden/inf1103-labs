62a29629d4cfbb12a12b54a264ee232c772eed20
eden li <edenlitx@gmail.com>
2026-09-16 11:48:29 +0800

    audit code fix

--- auditor.py
+++ auditor.py
@@ -1,14 +1,16 @@
 inventory = 0
-while inventory < 5:
+failedEntries = 0
+while inventory < 500:
     stock = input("Enter stock ")
     if stock == "quit":
+        print("Total Units Processed:", inventory)
+        print("Number of Failed/Rejected Entries:", failedEntries)
         break
-    elif stock.isdigit() == False:
-        print("Error: not accepted input, try again")
+    elif stock.isdigit() == False: #this for rejecting invalid input like ten or negative num
+        failedEntries = failedEntries + 1
+        print("Error: not an accepted input, try again") 
         continue
     inventory = inventory + int(stock)
-    print(inventory)
-if inventory < 5:
-    print("ended")
-else:
-    print("inventory more than 5")
+    print("Total Inventory:", inventory)
+if inventory > 500:
+    print("Inventory more than 500")
